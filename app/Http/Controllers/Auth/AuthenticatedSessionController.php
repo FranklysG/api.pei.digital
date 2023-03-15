813c820eebe50cab8e976e5bbc26c8aa3bd481e7
FranklysG <tunele095@gmail.com>
2023-03-15 20:23:35 -0300

    remove destroy session

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -41,11 +41,6 @@ class AuthenticatedSessionController extends Controller
     public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
         return response()->noContent();
     }
 }
